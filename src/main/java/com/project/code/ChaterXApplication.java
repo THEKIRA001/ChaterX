1dfa0e56ba3fdd4a027a4dba8250109edeff85a5
Shubham Bolave <shubhambolave2@gmail.com>
2023-08-15 22:55:24 +0530

    initialize

---
+++ src/main/java/com/project/code/ChaterXApplication.java
@@ -0,0 +1,14 @@
+package com.project.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaterXApplication {
+
+	public static void main(String[] args) {
+		System.out.println("working");
+		SpringApplication.run(ChaterXApplication.class, args);
+	}
+
+}
