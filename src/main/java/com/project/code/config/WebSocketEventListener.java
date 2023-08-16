b5cee0d8df791a7394cfc56974de03a80f0b29f7
Shubham Bolave <shubhambolave2@gmail.com>
2023-08-16 23:23:38 +0530

    16Aug-1

--- src/main/java/com/project/code/config/WebSocketEventListener.java
+++ src/main/java/com/project/code/config/WebSocketEventListener.java
@@ -1,17 +1,24 @@
 package com.project.code.config;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import com.project.code.chat.ChatMessage;
+import com.project.code.chat.MessageType;
+
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.var;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
+	
+	private final SimpMessageSendingOperations messageTemplate;
 
 	@EventListener
 	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
@@ -19,6 +26,10 @@ public class WebSocketEventListener {
 		String username = (String) headerAccessor.getSessionAttributes().get("username");
 		if(username != null) {
 			log.info("User disconnected: {}",username);
+			
+			@SuppressWarnings("deprecation")
+			var chatMessage = ChatMessage.builder().type(MessageType.LEAVE).sender(username).build();
+			messageTemplate.convertAndSend("topic/public",chatMessage);
 		}
 	}
 }
